41eebd22d211c6ff4aaf6077dccd19cc8fb0e6be
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-02-10 09:58:57 +0000

    using select to control a buffered channel

---
+++ phase_4/go.mod
@@ -0,0 +1,3 @@
+module pattern-4
+
+go 1.23.4
---
+++ phase_4/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan int, 2)
+	exit := make(chan struct{})
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			fmt.Println(time.Now(), i, "sending")
+			ch <- i
+			fmt.Println(time.Now(), i, "sent")
+
+			time.Sleep(time.Second * 1)
+		}
+
+		fmt.Println(time.Now(), "all completed, leaving")
+
+		close(ch)
+	}()
+
+	// Below is the consumer
+	go func() {
+		// N.B This is overcomplicated becos its a only one channel,
+		// select shines when using multiple channels
+		for {
+			select {
+			case v, open := <-ch:
+				if !open {
+					close(exit)
+					return
+				}
+				fmt.Println(time.Now(), "received", v)
+
+				// default:
+				// 	fmt.Println("Nothing is happening")
+			}
+		}
+	}()
+
+	fmt.Println(time.Now(), "waiting for everything to complete")
+
+	<-exit
+
+	fmt.Println(time.Now(), "exiting")
+}
